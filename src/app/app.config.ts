b8737fffff753058833e955bbfc9aac45b2e5907
Aya Adel <ayaadelyahoo66@gmail.com>
2026-02-18 23:33:11 +0200

    add toater and edit in homeComponent

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,24 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withViewTransitions(),
+    ),
     provideHttpClient(withFetch()),
+    provideAnimations(),
+    provideToastr(),
   ],
 };
--- src/app/feature/contact/components/form-contact/form-contact.component.ts
+++ src/app/feature/contact/components/form-contact/form-contact.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-form-contact',
   imports: [ReactiveFormsModule],
@@ -10,23 +11,31 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class FormContactComponent {
   private readonly httpClient = inject(HttpClient);
   private readonly fb = inject(FormBuilder);
+  private readonly toaster = inject(ToastrService);
+  loading = false;
   contactForm: FormGroup = this.fb.group({
-    name: [null, [Validators.required, Validators.maxLength(3)]],
+    name: [null, [Validators.required, Validators.minLength(3)]],
     email: [null, [Validators.email, Validators.required]],
     subject: [null, [Validators.required]],
     message: [null],
-    access_key: [`5d972dec-15f0-47f4-a5be-7540b0806513`, [Validators.required]],
+    access_key: [`5d972dec-15f0-47f4-a5be-7540b0806513`],
   });
   sendData() {
     const endPoint = `https://api.web3forms.com/submit`;
-    this.httpClient.post(endPoint, this.contactForm.value).subscribe({
-      next: (resp) => {
-        console.log(resp);
-      },
-      error: (err: HttpErrorResponse) => {
-        console.log(err.error.error);
-      },
-    });
-    console.log(this.contactForm.value);
+    if (this.contactForm.valid) {
+      this.loading = true;
+      this.httpClient.post<any>(endPoint, this.contactForm.value).subscribe({
+        next: (resp) => {
+          this.loading = false;
+          this.toaster.success(resp.message, '', {
+            progressBar: true,
+          });
+        },
+        error: (err: HttpErrorResponse) => {
+          this.toaster.error(err.error.error);
+          this.loading = false;
+        },
+      });
+    }
   }
 }
